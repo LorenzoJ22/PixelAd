da4d6938003322fe2ca8eaf435616560cc705dd3
LorenzoJ22 <lor.tenore@stud.uniroma3.it>
2023-11-09 17:33:15 +0100

    Video_4

---
+++ pixel_adventure/lib/components/player_hitbox.dart
@@ -0,0 +1,14 @@
+class PlayerHitBox {
+  final double offsetX;
+  final double offsetY;
+  final double width;
+  final double height;
+
+  PlayerHitBox({
+    required this.offsetX, 
+    required this.offsetY, 
+    required this.width, 
+    required this.height
+  });
+}
+
