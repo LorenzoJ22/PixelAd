f1f79b516f778645b0e359927fe2d4f18bac4623
LorenzoJ22 <lor.tenore@stud.uniroma3.it>
2023-12-12 13:07:20 +0100

    video 5 incompleto (minuto 36:00)

--- pixel_adventure/lib/components/level.dart
+++ pixel_adventure/lib/components/level.dart
@@ -4,11 +4,14 @@ import 'dart:io';
 
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/components/background_tile.dart';
 import 'package:pixel_adventure/components/collision_block.dart';
+import 'package:pixel_adventure/components/fruit.dart';
 //import 'package:flutter/foundation.dart';
 import 'package:pixel_adventure/components/player.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
 
-class Level extends World {
+class Level extends World with HasGameRef<PixelAdventure>{
   final String levelName;
   final Player player;
   Level({required this.levelName, required this.player});
@@ -21,7 +24,42 @@ class Level extends World {
     
     add(level);
 
-    final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+    _scrollingBackground();
+    _spawningObjects();
+    _addCollisions();
+
+  return super.onLoad();
+  }
+  
+  void _scrollingBackground() {
+    final backgroundLayer = level.tileMap.getLayer('Background');
+    const tileSize = 64;
+
+    final numTilesY = (game.size.y / tileSize).floor();
+    final numTilesX = (game.size.x / tileSize).floor();
+
+
+    if(backgroundLayer != null){
+      final backgroundColor = 
+        backgroundLayer.properties.getValue('BackgroundColor');
+
+      for(double y = 0; y < numTilesY+2; y++){
+        for(double x = 0; x < numTilesX; x++){
+          final backgroundTile = BackgroundTile(
+            color: backgroundColor ?? 'Gray', 
+            position: Vector2(x * tileSize, y * tileSize - tileSize),
+          );
+
+          add(backgroundTile);
+        }
+       
+      }
+
+    }
+  }
+  
+  void _spawningObjects() {
+     final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
 
     if(spawnPointsLayer != null) {
       for(final spawnPoint in spawnPointsLayer!.objects) {
@@ -30,12 +68,18 @@ class Level extends World {
           player.position = Vector2(spawnPoint.x, spawnPoint.y);
           add(player);
           break;
+        case 'Fruit': 
+        final fruit = Fruit(
+          
+        );
         default:
       }
     } 
   }
+}
   
-  final collisionsLayer = level.tileMap.getLayer<ObjectGroup>('Collisions');
+  void _addCollisions() {
+     final collisionsLayer = level.tileMap.getLayer<ObjectGroup>('Collisions');
 
   if(collisionsLayer != null) {
     for(final collision in collisionsLayer.objects ){
@@ -60,6 +104,5 @@ class Level extends World {
     }
   }
   player.collisionBlocks = collisionBlocks;
-  return super.onLoad();
   }
 }
